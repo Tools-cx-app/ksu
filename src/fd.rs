3fc234ee7bddb1acbb72c0775b2b5ee99af4890c
Tools-app <localhost.hutao@gmail.com>
2026-01-17 06:38:00 +0800

    opt: Get fd directly using syscall
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/fd.rs
+++ src/fd.rs
@@ -1,31 +1,11 @@
-use std::{fs, os::fd::RawFd, sync::OnceLock};
+use std::{os::fd::RawFd, sync::OnceLock};
 
 use crate::magic::{KSU_INSTALL_MAGIC1, KSU_INSTALL_MAGIC2};
 
 static DRIVER_FD: OnceLock<RawFd> = OnceLock::new();
 
-fn scan_driver_fd() -> Option<RawFd> {
-    let fd_dir = fs::read_dir("/proc/self/fd").ok()?;
-
-    for entry in fd_dir.flatten() {
-        if let Ok(fd_num) = entry.file_name().to_string_lossy().parse::<i32>() {
-            let link_path = format!("/proc/self/fd/{fd_num}");
-            if let Ok(target) = fs::read_link(&link_path) {
-                let target_str = target.to_string_lossy();
-                if target_str.contains("[ksu_driver]") {
-                    return Some(fd_num);
-                }
-            }
-        }
-    }
-
-    None
-}
-
-// Get cached driver fd
-fn init_driver_fd() -> Option<RawFd> {
-    let fd = scan_driver_fd();
-    if fd.is_none() {
+pub(crate) fn get_fd() -> RawFd {
+    *DRIVER_FD.get_or_init(|| {
         let mut fd = -1;
         unsafe {
             libc::syscall(
@@ -36,12 +16,6 @@ fn init_driver_fd() -> Option<RawFd> {
                 &mut fd,
             );
         };
-        if fd >= 0 { Some(fd) } else { None }
-    } else {
         fd
-    }
-}
-
-pub(crate) fn get_fd() -> RawFd {
-    *DRIVER_FD.get_or_init(|| init_driver_fd().unwrap_or(-1))
+    })
 }
